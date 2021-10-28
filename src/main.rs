58bf974e98b824d7e7f4d8a92b8e1510c677624d
Jack Baron <jackmbaron@gmail.com>
2021-10-28 15:09:36 +0100

    add tracing logger

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 use clap::Parser;
+use tracing::metadata::LevelFilter;
 
 #[derive(Debug, Parser)]
 struct Args {
     /// PostgreSQL Connection String
     #[clap(short, long = "psql", env = "WQL_POSTGRES_CONNECTION_STRING")]
     postgres_connection_string: String,
+
+    /// Enable Debug Logs
+    #[clap(long, env = "WQL_DEBUG")]
+    debug: bool,
 }
 
 fn main() {
     let args = Args::parse();
-    dbg!(&args);
+
+    // Always enable TRACE level logging on debug builds
+    #[cfg(debug_assertions)]
+    let filter = LevelFilter::TRACE;
+    // Conditionally enable DEBUG/INFO logging on release builds
+    #[cfg(not(debug_assertions))]
+    let filter = match args.debug {
+        true => LevelFilter::DEBUG,
+        false => LevelFilter::INFO,
+    };
+
+    tracing_subscriber::fmt().with_max_level(filter).init();
 }
