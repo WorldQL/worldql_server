d7b04dd9cda36c9e4d04153c8077e3b0e3e7bf9a
Jack Baron <jackmbaron@gmail.com>
2021-10-28 16:33:24 +0100

    start implementing deserialization

--- src/structures/message.rs
+++ src/structures/message.rs
@@ -1,7 +1,8 @@
-use flatbuffers::FlatBufferBuilder;
+use flatbuffers::{FlatBufferBuilder, InvalidFlatbuffer};
+use thiserror::Error;
 
 use super::{Entity, Record, Vec3D};
-use crate::flatbuffers::{Message as MessageFB, MessageArgs};
+use crate::flatbuffers::{Message as MessageFB, MessageArgs, root_as_message};
 
 #[derive(Debug, Default)]
 pub struct Message {
@@ -45,4 +46,42 @@ impl Message {
 
         bytes.to_vec()
     }
+
+    pub fn deserialize(buf: &[u8]) -> Result<Self, DeserializeError> {
+        let raw_message = root_as_message(buf)?;
+
+        // Validate required fields
+        let instruction = raw_message
+            .instruction()
+            .ok_or(DeserializeError::MissingRequiredField("instruction".into()))?
+            .into();
+        let sender_uuid = raw_message
+            .sender_uuid()
+            .ok_or(DeserializeError::MissingRequiredField("sender_uuid".into()))?
+            .into();
+        let world_name = raw_message
+            .world_name()
+            .ok_or(DeserializeError::MissingRequiredField("world_name".into()))?
+            .into();
+
+        let message = Message {
+            instruction,
+            sender_uuid,
+            world_name,
+
+            // TODO: Rest of the fields
+            ..Default::default()
+        };
+
+        Ok(message)
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum DeserializeError {
+    #[error("missing required field: {0}")]
+    MissingRequiredField(String),
+
+    #[error(transparent)]
+    InvalidFlatbuffer(#[from] InvalidFlatbuffer),
 }
