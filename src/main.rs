cabfcff5ebd20b766cd5da514d55f9c8433515ba
Jack Baron <jackmbaron@gmail.com>
2021-10-28 14:57:21 +0100

    setup args/env parsing

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// PostgreSQL Connection String
+    #[clap(short, long = "psql", env = "WQL_POSTGRES_CONNECTION_STRING")]
+    postgres_connection_string: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    dbg!(&args);
+}
