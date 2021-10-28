cef4140ac15cd900e37d75cc124ada8e27fd5fc1
Jack Baron <jackmbaron@gmail.com>
2021-10-28 15:23:41 +0100

    import flatbuffer glue code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use clap::Parser;
 use tracing::metadata::LevelFilter;
 
+#[allow(dead_code, unused_imports)]
+#[path = "./WorldQLFB_generated.rs"]
+mod worldql_flatbuffers;
+
 mod structures;
 
 #[derive(Debug, Parser)]
