009a55e34917625078c869593c31002b954f3038
Jack Baron <jackmbaron@gmail.com>
2021-11-02 12:05:52 +0000

    parse zmq messages and drop where invalid

--- src/transport/zeromq.rs
+++ src/transport/zeromq.rs
@@ -1,6 +1,7 @@
 use color_eyre::Result;
+use futures_util::StreamExt;
 use tokio::sync::mpsc::UnboundedSender;
-use tracing::info;
+use tracing::{info, trace};
 
 use super::ThreadPeerMap;
 use crate::structures::Message;
@@ -18,6 +19,33 @@ pub async fn start_zeromq_server(
     let mut pull_socket = tmq::pull(&ctx).bind(&pull_addr)?;
     info!("ZeroMQ PULL Server listening on port {}", server_port);
 
-    // TODO
+    loop {
+        let msg = pull_socket.next().await;
+        match msg {
+            None => break,
+            Some(msg) => {
+                let msg = msg?;
+                if msg.len() != 1 {
+                    trace!("dropping multipart zmq message");
+                    continue;
+                }
+
+                // Take first element from multipart
+                let data = msg.into_iter().next().unwrap();
+                let message_result = Message::deserialize(&data);
+
+                if message_result.is_err() {
+                    trace!("dropping invalid zmq message");
+                    continue;
+                }
+
+                let message = message_result.unwrap();
+
+                // TODO: Handle handshakes and message forwarding
+                dbg!(&message);
+            }
+        }
+    }
+
     Ok(())
 }
