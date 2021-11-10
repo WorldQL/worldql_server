98f7f597e923b815f86c803dc20c9dbecf6ebf85
Jack Baron <jackmbaron@gmail.com>
2021-11-10 19:35:43 +0000

    overhaul documentation script

--- view_documentation.sh
+++ view_documentation.sh
@@ -1,4 +1,6 @@
+# Generate Documentation
 cargo doc
-echo "http://127.0.0.1:8000/worldql_server/index.html"
-cd target/doc && python3 -m http.server -b localhost
 
+# Start Webserver
+echo "http://127.0.0.1:8000/worldql_server/index.html"
+python3 -m http.server -b localhost -d ./target/doc
