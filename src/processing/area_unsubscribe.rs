8ed976f9070a53ddefee0a2028751ed7a2c76739
Jack Baron <jackmbaron@gmail.com>
2021-11-12 17:33:30 +0000

    impl area (un)subscribe

--- src/processing/area_unsubscribe.rs
+++ src/processing/area_unsubscribe.rs
@@ -1,4 +1,5 @@
 use color_eyre::Result;
+use tracing::debug;
 
 use crate::structures::Message;
 use crate::subscriptions::AreaMap;
@@ -9,6 +10,16 @@ pub async fn handle_area_unsubscribe(
     peer_map: &ThreadPeerMap,
     area_map: &mut AreaMap,
 ) -> Result<()> {
-    // TODO
-    todo!()
+    let uuid = message.sender_uuid;
+    let cube = match message.position {
+        Some(pos) => pos,
+        None => {
+            debug!("invalid AreaSubscribe from peer {}, missing position", &uuid);
+            return Ok(())
+        }
+    };
+
+    area_map.remove_subscription(&uuid, cube);
+
+    Ok(())
 }
