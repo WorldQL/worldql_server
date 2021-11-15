724d0f44c34c8689e04ee321a8ed545380e60dc8
Jack Baron <jackmbaron@gmail.com>
2021-11-15 13:28:02 +0000

    never allow packet tracing on release builds

--- src/utils/trace_packet.rs
+++ src/utils/trace_packet.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! trace_packet {
     ($target: expr, $($field:tt)*) => {
-        #[cfg(feature = "trace_packets")]
+        #[cfg(all(debug_assertions, feature = "trace_packets"))]
         tracing::trace!($target, $($field)*);
     };
 }
