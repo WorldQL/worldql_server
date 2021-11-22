50fafc3fd87610482859e1d7d02fcdd94c23cb4a
Jack Baron <jackmbaron@gmail.com>
2021-11-22 11:27:07 +0000

    add public API method skeletons

--- src/database/client.rs
+++ src/database/client.rs
@@ -1,4 +1,7 @@
+use color_eyre::Result;
 use tokio_postgres::Client;
+
+use crate::structures::{Record, Vector3};
 
 pub struct DatabaseClient {
     pub(super) client: Client,
@@ -7,5 +10,13 @@ pub struct DatabaseClient {
 impl DatabaseClient {
     pub fn new(client: Client) -> Self {
         Self { client }
+    }
+
+    pub async fn create_record(record: Record) -> Result<()> {
+        todo!()
+    }
+
+    pub async fn records_in_region(world_name: &str, point_inside_region: Vector3) -> Result<()> {
+        todo!()
     }
 }
