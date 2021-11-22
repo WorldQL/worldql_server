a4e6e71ed0f8005258bc0ed71b9172ee6857cb65
Jack Baron <jackmbaron@gmail.com>
2021-11-22 12:13:40 +0000

    add caches in databaseclient

--- src/database/client.rs
+++ src/database/client.rs
@@ -1,10 +1,15 @@
+use std::collections::HashMap;
+
 use color_eyre::Result;
 use tokio_postgres::Client;
 
+use super::world_region::WorldRegion;
 use crate::structures::{Record, Vector3};
 
 pub struct DatabaseClient {
     pub(super) client: Client,
+    pub(super) table_cache: HashMap<WorldRegion, i32>,
+    pub(super) region_cache: HashMap<WorldRegion, i32>,
 
     region_x_size: u16,
     region_y_size: u16,
@@ -13,9 +18,17 @@ pub struct DatabaseClient {
 }
 
 impl DatabaseClient {
-    pub fn new(client: Client, region_x_size: u16, region_y_size: u16, region_z_size: u16, table_size: u32) -> Self {
+    pub fn new(
+        client: Client,
+        region_x_size: u16,
+        region_y_size: u16,
+        region_z_size: u16,
+        table_size: u32,
+    ) -> Self {
         Self {
             client,
+            table_cache: HashMap::new(),
+            region_cache: HashMap::new(),
 
             region_x_size,
             region_y_size,
