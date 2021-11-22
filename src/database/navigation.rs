93663e1a7a1ecc71c14286ab18d068000f8737fd
Jack Baron <jackmbaron@gmail.com>
2021-11-22 12:57:33 +0000

    start impl lookup for table_suffix

--- src/database/navigation.rs
+++ src/database/navigation.rs
@@ -1,35 +1,64 @@
+use tokio_postgres::Error;
+
 use super::world_region::WorldRegion;
 use super::DatabaseClient;
+use crate::database::LOOKUP_TABLE_SUFFIX;
 use crate::structures::Vector3;
 
 impl DatabaseClient {
     /// Lookup both `table_id` and `region_id` in a single function.
     ///
     /// Returned tuple has the form `(table_id, region_id)`
-    pub(super) async fn lookup_ids(&mut self, world_name: &str, point: Vector3) -> (i32, i32) {
+    pub(super) async fn lookup_ids(
+        &mut self,
+        world_name: &str,
+        point: Vector3,
+    ) -> Result<(i32, i32), Error> {
         let world_region = self.world_region(world_name, point);
-        let table_id = self.get_table_id(&world_region).await;
-        let region_id = self.get_region_id(&world_region).await;
+        let table_id = self.get_table_suffix(&world_region).await?;
+        let region_id = self.get_region_id(&world_region).await?;
 
-        (table_id, region_id)
+        Ok((table_id, region_id))
     }
 
-    async fn get_table_id(&mut self, region: &WorldRegion) -> i32 {
+    async fn get_table_suffix(&mut self, region: &WorldRegion) -> Result<i32, Error> {
         // Early return for cached value
         if let Some(id) = self.table_cache.get(region) {
-            return *id;
+            return Ok(*id);
         }
 
-        todo!()
+        // Query database for table_suffix
+        let rows = self
+            .client
+            .query(
+                LOOKUP_TABLE_SUFFIX,
+                &[region.world_name(), region.x(), region.y(), region.z()],
+            )
+            .await?;
+
+        let table_suffix = match rows.get(0) {
+            // Suffix found, return
+            Some(row) => {
+                // TODO: Return table_suffix
+                todo!()
+            }
+
+            // No suffix found, create and return
+            None => {
+                // TODO:
+                todo!()
+            }
+        };
+
+        // Insert into cache and return
+        self.table_cache.insert(region.clone(), table_suffix);
+        return Ok(table_suffix)
     }
 
-    async fn get_region_id(
-        &mut self,
-        region: &WorldRegion,
-    ) -> i32 {
+    async fn get_region_id(&mut self, region: &WorldRegion) -> Result<i32, Error> {
         // Early return for cached value
         if let Some(id) = self.region_cache.get(region) {
-            return *id;
+            return Ok(*id);
         }
 
         todo!()
--- src/database/query_const.rs
+++ src/database/query_const.rs
@@ -1,29 +1,55 @@
 // region: Init
 pub(super) const CREATE_TABLE_NAVIGATION: &str = "
-CREATE TABLE IF NOT EXISTS table_navigation
-(
-   min_x        integer,
-   max_x        integer,
-   min_y        integer,
-   max_y        integer,
-   min_z        integer,
-   max_z        integer,
-   world_name   varchar(32),
-   table_suffix serial
-);
+    CREATE TABLE IF NOT EXISTS table_navigation
+    (
+        min_x        integer,
+        max_x        integer,
+        min_y        integer,
+        max_y        integer,
+        min_z        integer,
+        max_z        integer,
+        world_name   varchar(32),
+        table_suffix serial
+    );
 ";
 
 pub(super) const CREATE_REGION_NAVIGATION: &str = "
-CREATE TABLE IF NOT EXISTS region_navigation
-(
-   min_x      integer,
-   max_x      integer,
-   min_y      integer,
-   max_y      integer,
-   min_z      integer,
-   max_z      integer,
-   world_name varchar(32),
-   region_id  serial
-);
+    CREATE TABLE IF NOT EXISTS region_navigation
+    (
+        min_x      integer,
+        max_x      integer,
+        min_y      integer,
+        max_y      integer,
+        min_z      integer,
+        max_z      integer,
+        world_name varchar(32),
+        region_id  serial
+    );
 ";
+// endregion
+
+// region: Lookups
+pub(super) const LOOKUP_TABLE_SUFFIX: &str = "
+    SELECT table_suffix FROM table_navigation
+    WHERE world_name = $1 AND
+    $2 >= min_x AND $2 < max_x AND
+    $3 >= min_y AND $3 < max_y AND
+    $4 >= min_z AND $4 < max_z
+";
+
+pub(super) const LOOKUP_REGION_ID: &str = "
+    SELECT region_id FROM region_navigation
+    WHERE world_name = $1 AND
+    $2 >= min_x AND $2 < max_x AND
+    $3 >= min_y AND $3 < max_y AND
+    $4 >= min_z AND $4 < max_z
+";
+// endregion
+
+// region: Inserts
+// TODO
+pub(super) const INSERT_TABLE_SUFFIX: &str = "";
+
+// TODO
+pub(super) const INSERT_REGION_ID: &str = "";
 // endregion
