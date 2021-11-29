c7035e50cd170d96f3beb33d5d7ac7e015160b36
Jack Baron <jackmbaron@gmail.com>
2021-11-29 15:29:51 +0000

    multithread processing

--- src/processing/thread.rs
+++ src/processing/thread.rs
@@ -1,6 +1,6 @@
 use color_eyre::Result;
-use flume::Receiver;
-use tracing::{debug, warn};
+use flume::{Receiver, Sender};
+use tracing::warn;
 use uuid::Uuid;
 
 use super::area_subscribe::handle_area_subscribe as area_subscribe;
@@ -16,10 +16,100 @@ use crate::transport::ThreadPeerMap;
 use crate::{trace_packet, DatabaseClient};
 
 pub async fn start_processing_thread(
-    mut database_client: DatabaseClient,
+    database_client: DatabaseClient,
     peer_map: ThreadPeerMap,
     msg_rx: Receiver<Message>,
     remove_rx: Receiver<Uuid>,
+    cube_size: u16,
+) -> Result<()> {
+    let (sub_tx, sub_rx) = flume::unbounded();
+    let (db_tx, db_rx) = flume::unbounded();
+
+    let mut db = tokio::spawn(handle_db_messages(db_rx, peer_map.clone(), database_client));
+    let mut sub = tokio::spawn(handle_sub_messages(
+        sub_rx,
+        remove_rx,
+        peer_map.clone(),
+        cube_size,
+    ));
+
+    loop {
+        tokio::select! {
+            // Handle incoming messages
+            Ok(message) = msg_rx.recv_async() => {
+                handle_message(&sub_tx, &db_tx, &peer_map, message).await?;
+            },
+
+            // Exit early if sub processing errors
+            Ok(Err(error)) = &mut sub => {
+                return Err(error);
+            },
+
+            // Exit early if DB processing errors
+            Ok(Err(error)) = &mut db => {
+                return Err(error);
+            },
+
+            // Both channels have closed, exit thread
+            else => break,
+        }
+    }
+
+    Ok(())
+}
+
+#[inline]
+async fn handle_message(
+    sub_tx: &Sender<Message>,
+    db_tx: &Sender<Message>,
+    peer_map: &ThreadPeerMap,
+    message: Message,
+) -> Result<()> {
+    match message.instruction {
+        // Panic on handshakes, they should never be sent to this thread.
+        Instruction::Handshake => panic!("recieved handshake instruction on processing thread"),
+
+        // Panic on incoming client-bound instructions
+        Instruction::PeerConnect | Instruction::PeerDisconnect | Instruction::RecordReply => {
+            panic!("received incoming client-bound instruction")
+        }
+
+        // Instantly handle heartbeats
+        Instruction::Heartbeat => heartbeat(message, peer_map).await?,
+
+        // Handle subscription messages
+        Instruction::AreaSubscribe
+        | Instruction::AreaUnsubscribe
+        | Instruction::GlobalMessage
+        | Instruction::LocalMessage => {
+            sub_tx.send_async(message).await?;
+        }
+
+        // Handle database messages
+        Instruction::RecordCreate
+        | Instruction::RecordRead
+        | Instruction::RecordUpdate
+        | Instruction::RecordDelete => {
+            db_tx.send_async(message).await?;
+        }
+
+        // Warn on unknown instructions
+        Instruction::Unknown => {
+            let map = peer_map.read().await;
+            let peer = map.get(&message.sender_uuid).unwrap();
+
+            warn!("Unknown Instruction received from {}", peer);
+            trace_packet!("{}", message);
+        }
+    }
+
+    Ok(())
+}
+
+async fn handle_sub_messages(
+    msg_rx: Receiver<Message>,
+    remove_rx: Receiver<Uuid>,
+    peer_map: ThreadPeerMap,
     cube_size: u16,
 ) -> Result<()> {
     let mut world_map = WorldMap::new(cube_size);
@@ -33,7 +123,14 @@ pub async fn start_processing_thread(
 
             // Handle incoming messages
             Ok(message) = msg_rx.recv_async() => {
-                handle_message(message, &mut database_client, &peer_map, &mut world_map).await?;
+                match message.instruction {
+                    Instruction::AreaSubscribe => area_subscribe(message, &peer_map, &mut world_map)?,
+                    Instruction::AreaUnsubscribe => area_unsubscribe(message, &peer_map, &mut world_map)?,
+                    Instruction::LocalMessage => local_message(message, &peer_map, &world_map).await?,
+                    Instruction::GlobalMessage => global_message(message, &peer_map, &world_map).await?,
+
+                    _ => panic!("invalid message type"),
+                }
             },
 
             // Both channels have closed, exit thread
@@ -44,38 +141,31 @@ pub async fn start_processing_thread(
     Ok(())
 }
 
-#[inline]
-async fn handle_message(
-    message: Message,
-    database_client: &mut DatabaseClient,
-    peer_map: &ThreadPeerMap,
-    world_map: &mut WorldMap,
+async fn handle_db_messages(
+    msg_rx: Receiver<Message>,
+    peer_map: ThreadPeerMap,
+    mut database_client: DatabaseClient,
 ) -> Result<()> {
-    match message.instruction {
-        // Panic on handshakes, they should never be sent to this thread.
-        Instruction::Handshake => panic!("recieved handshake instruction on processing thread"),
+    loop {
+        let message = msg_rx.recv_async().await?;
+        match message.instruction {
+            Instruction::RecordCreate => {
+                record_create(message, &mut database_client, &peer_map).await?
+            }
 
-        // Handle known instructions
-        Instruction::Heartbeat => heartbeat(message, peer_map).await?,
-        Instruction::AreaSubscribe => area_subscribe(message, peer_map, world_map)?,
-        Instruction::AreaUnsubscribe => area_unsubscribe(message, peer_map, world_map)?,
-        Instruction::LocalMessage => local_message(message, peer_map, world_map).await?,
-        Instruction::GlobalMessage => global_message(message, peer_map, world_map).await?,
-        Instruction::RecordCreate => record_create(message, database_client, peer_map).await?,
-        Instruction::RecordRead => record_read(message, database_client, peer_map).await?,
+            Instruction::RecordRead => {
+                record_read(message, &mut database_client, &peer_map).await?
+            }
 
-        // Warn on unknown instructions
-        Instruction::Unknown => {
-            let map = peer_map.read().await;
-            let peer = map.get(&message.sender_uuid).unwrap();
+            Instruction::RecordUpdate => {
+                todo!()
+            }
 
-            warn!("Unknown Instruction received from {}", peer);
-            trace_packet!("{}", message);
+            Instruction::RecordDelete => {
+                todo!()
+            }
+
+            _ => panic!("invalid message type"),
         }
-
-        // Emit debug message for unhandled instructions
-        _ => debug!("unhandled instruction: {:?}", message.instruction),
     }
-
-    Ok(())
 }
