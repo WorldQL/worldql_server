0509968415721fcec826979ea73d651a8925d9f3
Jack Baron <jackmbaron@gmail.com>
2022-01-21 00:31:26 +0000

    format

--- worldql_server/src/main.rs
+++ worldql_server/src/main.rs
@@ -1,4 +1,10 @@
-#![warn(clippy::cast_lossless, clippy::implicit_clone, clippy::unused_async, clippy::missing_panics_doc, clippy::redundant_closure_for_method_calls)]
+#![warn(
+    clippy::cast_lossless,
+    clippy::implicit_clone,
+    clippy::unused_async,
+    clippy::missing_panics_doc,
+    clippy::redundant_closure_for_method_calls
+)]
 
 use std::collections::HashSet;
 use std::sync::Arc;
