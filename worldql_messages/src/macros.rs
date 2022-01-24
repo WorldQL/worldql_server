c8667f2d0eec7c691ddb3202e6aa6aef829081dd
Jack Baron <jackmbaron@gmail.com>
2022-01-24 15:09:37 +0000

    impl converting directly into outgoing message

--- worldql_messages/src/macros.rs
+++ worldql_messages/src/macros.rs
@@ -14,3 +14,20 @@ macro_rules! impl_into_super {
 }
 
 pub(crate) use impl_into_super;
+
+macro_rules! impl_into_message {
+    ($from:ident, $variant:ident, $super:ty) => {
+        paste::paste! {
+            #[automatically_derived]
+            impl From<[< $from $variant >]> for $super {
+                #[inline]
+                #[must_use]
+                fn from(from: [< $from $variant >]) -> Self {
+                    Self::$variant(from.into())
+                }
+            }
+        }
+    };
+}
+
+pub(crate) use impl_into_message;
--- worldql_messages/src/outgoing/event/mod.rs
+++ worldql_messages/src/outgoing/event/mod.rs
@@ -4,7 +4,7 @@ pub use self::global_message_event::GlobalMessageEvent;
 pub use self::local_message_event::LocalMessageEvent;
 pub use self::peer_connect_event::PeerConnectEvent;
 pub use self::peer_disconnect_event::PeerDisconnectEvent;
-use crate::macros::impl_into_super;
+use crate::macros::{impl_into_message, impl_into_super};
 
 mod global_message_event;
 mod local_message_event;
@@ -25,3 +25,8 @@ impl_into_super!(PeerConnect, Event, OutgoingMessageEvent);
 impl_into_super!(PeerDisconnect, Event, OutgoingMessageEvent);
 impl_into_super!(GlobalMessage, Event, OutgoingMessageEvent);
 impl_into_super!(LocalMessage, Event, OutgoingMessageEvent);
+
+impl_into_message!(PeerConnect, Event, super::OutgoingMessage);
+impl_into_message!(PeerDisconnect, Event, super::OutgoingMessage);
+impl_into_message!(GlobalMessage, Event, super::OutgoingMessage);
+impl_into_message!(LocalMessage, Event, super::OutgoingMessage);
