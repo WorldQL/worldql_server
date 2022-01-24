0b90a315839e96f7450c5602864537aa7118cc71
Jack Baron <jackmbaron@gmail.com>
2022-01-24 15:23:45 +0000

    add more lints

--- worldql_messages/src/lib.rs
+++ worldql_messages/src/lib.rs
@@ -1,4 +1,33 @@
 #![forbid(unsafe_code)]
+#![deny(private_in_public)]
+#![warn(
+    clippy::all,
+    clippy::dbg_macro,
+    clippy::todo,
+    clippy::empty_enum,
+    clippy::enum_glob_use,
+    clippy::unused_self,
+    clippy::needless_continue,
+    clippy::needless_borrow,
+    clippy::match_wildcard_for_single_variants,
+    clippy::if_let_mutex,
+    clippy::mismatched_target_os,
+    clippy::match_on_vec_items,
+    clippy::imprecise_flops,
+    clippy::suboptimal_flops,
+    clippy::lossy_float_literal,
+    clippy::fn_params_excessive_bools,
+    clippy::inefficient_to_string,
+    clippy::macro_use_imports,
+    clippy::option_option,
+    clippy::unnested_or_patterns,
+    clippy::str_to_string,
+    rust_2018_idioms,
+    future_incompatible,
+    nonstandard_style,
+    missing_debug_implementations,
+    missing_docs
+)]
 
 pub mod common;
 pub mod incoming;
