83197a7d6aba8ab86606ce00bfedab60e69d627f
Jack Baron <jackmbaron@gmail.com>
2022-01-24 15:06:53 +0000

    impl converting event struct to event enum

--- worldql_messages/src/outgoing/event/mod.rs
+++ worldql_messages/src/outgoing/event/mod.rs
@@ -4,6 +4,7 @@ pub use self::global_message_event::GlobalMessageEvent;
 pub use self::local_message_event::LocalMessageEvent;
 pub use self::peer_connect_event::PeerConnectEvent;
 pub use self::peer_disconnect_event::PeerDisconnectEvent;
+use crate::macros::impl_into_super;
 
 mod global_message_event;
 mod local_message_event;
@@ -19,3 +20,8 @@ pub enum OutgoingMessageEvent {
     GlobalMessage(GlobalMessageEvent),
     LocalMessage(LocalMessageEvent),
 }
+
+impl_into_super!(PeerConnect, Event, OutgoingMessageEvent);
+impl_into_super!(PeerDisconnect, Event, OutgoingMessageEvent);
+impl_into_super!(GlobalMessage, Event, OutgoingMessageEvent);
+impl_into_super!(LocalMessage, Event, OutgoingMessageEvent);
