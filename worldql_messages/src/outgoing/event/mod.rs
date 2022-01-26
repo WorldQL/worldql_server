366eccb33b23608402b0a96f034da704b1f5e658
Jack Baron <jackmbaron@gmail.com>
2022-01-26 09:39:17 +0000

    add system message event type

--- worldql_messages/src/outgoing/event/mod.rs
+++ worldql_messages/src/outgoing/event/mod.rs
@@ -4,17 +4,22 @@ pub use self::global_message_event::GlobalMessageEvent;
 pub use self::local_message_event::LocalMessageEvent;
 pub use self::peer_connect_event::PeerConnectEvent;
 pub use self::peer_disconnect_event::PeerDisconnectEvent;
+pub use self::system_message::SystemMessageEvent;
 use crate::macros::{impl_into_message, impl_into_super};
 
 mod global_message_event;
 mod local_message_event;
 mod peer_connect_event;
 mod peer_disconnect_event;
+mod system_message;
 
 /// Events that are not tied to a request/reply pair
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(tag = "event", rename_all = "snake_case")]
 pub enum OutgoingMessageEvent {
+    /// System Message
+    SystemMessage(SystemMessageEvent),
+
     /// Peer Connect
     PeerConnect(PeerConnectEvent),
 
@@ -28,11 +33,13 @@ pub enum OutgoingMessageEvent {
     LocalMessage(LocalMessageEvent),
 }
 
+impl_into_super!(SystemMessage, Event, OutgoingMessageEvent);
 impl_into_super!(PeerConnect, Event, OutgoingMessageEvent);
 impl_into_super!(PeerDisconnect, Event, OutgoingMessageEvent);
 impl_into_super!(GlobalMessage, Event, OutgoingMessageEvent);
 impl_into_super!(LocalMessage, Event, OutgoingMessageEvent);
 
+impl_into_message!(SystemMessage, Event, super::OutgoingMessage);
 impl_into_message!(PeerConnect, Event, super::OutgoingMessage);
 impl_into_message!(PeerDisconnect, Event, super::OutgoingMessage);
 impl_into_message!(GlobalMessage, Event, super::OutgoingMessage);
---
+++ worldql_messages/src/outgoing/event/system_message.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+
+use crate::outgoing::Error;
+
+/// Emitted when the server needs to send a message to any clients
+///
+/// Might be sent globally or might be sent directly to a single client
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(tag = "message", rename_all = "snake_case")]
+pub enum SystemMessageEvent {
+    /// Indicates that an unknown error occurred, outside of any request / reply pairs
+    UnknownError(Error),
+
+    /// Indicates the connection is about to be terminated
+    Disconnect {
+        /// Reason for terminating the connection
+        reason: String,
+    }
+}
+
+impl SystemMessageEvent {
+    /// Create a new [`SystemMessageEvent`] with the reason set to [`UnknownError`](SystemMessageEvent::UnknownError)
+    #[inline]
+    #[must_use]
+    pub fn new_unknown_error(error: Error) -> Self {
+        Self::UnknownError(error)
+    }
+
+    /// Create a new [`SystemMessageEvent`] with the reason set to [`Disconnect`](SystemMessageEvent::Disconnect)
+    #[inline]
+    #[must_use]
+    pub fn new_disconnect(reason: impl Into<String>) -> Self {
+        Self::Disconnect { reason: reason.into() }
+    }
+}
