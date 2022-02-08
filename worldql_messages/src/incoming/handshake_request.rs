ac8eeb1c696bf1065b063730e1eb187930d3304f
Jack Baron <jackmbaron@gmail.com>
2022-02-08 03:58:16 +0000

    remove handshake options for now only targeting ws

--- worldql_messages/src/incoming/handshake_request.rs
+++ worldql_messages/src/incoming/handshake_request.rs
@@ -7,51 +7,13 @@ pub struct HandshakeRequest {
     ///
     /// Used if the WorldQL server requires authentication
     pub server_auth: Option<String>,
-
-    /// Additional transport-specific options
-    pub options: HandshakeOptions,
-}
-
-/// Options specific to each transport
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(tag = "options", rename_all = "snake_case")]
-pub enum HandshakeOptions {
-    /// Options specific to WebSocket connections
-    WebSocket {
-        // WebSocket connections currently have no additional options
-    },
-
-    /// Options specific to ZeroMQ connections
-    ZeroMQ {
-        /// The host string where the ZeroMQ client can be reached
-        push_host: String,
-    },
 }
 
 impl HandshakeRequest {
-    /// Create a new [`HandshakeRequest`] for a WebSocket connection
+    /// Create a new [`HandshakeRequest`]
     #[inline]
     #[must_use]
-    pub fn new_websocket(server_auth: Option<String>) -> Self {
-        let options = HandshakeOptions::WebSocket {};
-
-        Self {
-            server_auth,
-            options,
-        }
-    }
-
-    /// Create a new [`HandshakeRequest`] for a ZeroMQ connection
-    #[inline]
-    #[must_use]
-    pub fn new_zeromq(server_auth: Option<String>, push_host: impl Into<String>) -> Self {
-        let options = HandshakeOptions::ZeroMQ {
-            push_host: push_host.into(),
-        };
-
-        Self {
-            server_auth,
-            options,
-        }
+    pub fn new(server_auth: Option<String>) -> Self {
+        Self { server_auth }
     }
 }
