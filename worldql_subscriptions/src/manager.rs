a677608ce413e9b3bdf9e98461668e39419b6040
Jack Baron <jackmbaron@gmail.com>
2022-02-10 15:30:13 +0000

    mark manager methods as must_use

--- worldql_subscriptions/src/manager.rs
+++ worldql_subscriptions/src/manager.rs
@@ -1,42 +1,44 @@
 use uuid::Uuid;
 
+use crate::Area;
+
 #[derive(Debug)]
-pub struct SubscriptionManager {
-
-}
-
-type Area = ();
+pub struct SubscriptionManager {}
 
 impl SubscriptionManager {
+    #[must_use]
     pub fn is_subscribed_world(&self, peer: Uuid, world: &str) -> bool {
         todo!()
     }
 
+    #[must_use]
     pub fn is_subscribed_area(&self, peer: Uuid, world: &str, area: Area) -> bool {
         todo!()
     }
 
-    pub fn get_subscribed_to_world(&self, world: &str) -> impl Iterator<Item = Uuid> + '_ {
+    // #[must_use]
+    // pub fn get_subscribed_to_world(&self, world: &str) -> impl Iterator<Item = Uuid> + '_ {
+    //     todo!()
+    // }
+
+    // #[must_use]
+    // pub fn get_subscribed_to_area(&self, world: &str, area: Area) -> impl Iterator<Item = Uuid> + '_ {
+    //     todo!()
+    // }
+
+    pub fn subscribe_to_world(&mut self, peer: Uuid, world: impl Into<String>) {
         todo!()
     }
 
-    pub fn get_subscribed_to_area(&self, world: &str, area: Area) -> impl Iterator<Item = Uuid> + '_ {
+    pub fn unsubscribe_from_world(&mut self, peer: Uuid, world: &str) {
         todo!()
     }
 
-    pub fn subscribe_to_world(&mut self, peer: Uuid, world: impl Into<String>) -> bool {
+    pub fn subscribe_to_area(&mut self, peer: Uuid, world: impl Into<String>, area: Area) {
         todo!()
     }
 
-    pub fn unsubscribe_from_world(&mut self, peer: Uuid, world: &str) -> bool {
-        todo!()
-    }
-
-    pub fn subscribe_to_area(&mut self, peer: Uuid, world: impl Into<String>, area: Area) -> bool {
-        todo!()
-    }
-
-    pub fn unsubscribe_from_area(&mut self, peer: Uuid, world: &str, area: Area) -> bool {
+    pub fn unsubscribe_from_area(&mut self, peer: Uuid, world: &str, area: Area) {
         todo!()
     }
 
