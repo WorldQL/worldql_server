0a07b974c1cdf427138be5b9d7dd821a86f1bdce
Jack Baron <jackmbaron@gmail.com>
2022-02-10 17:08:02 +0000

    add more errors impl error function macro for dynamic error messages

--- worldql_server/src/errors.rs
+++ worldql_server/src/errors.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::unusual_byte_groupings)]
+
 use once_cell::sync::Lazy;
 use worldql_messages::client_bound::Error;
 
@@ -7,7 +9,30 @@ macro_rules! error {
     };
 }
 
-error!(ERR_INVALID_MESSAGE, 0x00001, "invalid message");
+macro_rules! error_func {
+    ($name: ident, $code:expr) => {
+        pub fn $name(message: impl Into<String>) -> Error {
+            Error::new($code, message)
+        }
+    };
+}
+
+// region: Generic Transport
+error!(ERR_INVALID_MESSAGE, 0x00_001, "invalid message");
 #[rustfmt::skip]
-error!(ERR_HANDSHAKE_REQUIRED, 0x00002, "first message must be a handshake");
-error!(ERR_DUPLICATE_UUID, 0x00003, "uuid already in use");
+error!(ERR_HANDSHAKE_REQUIRED, 0x00_002, "first message must be a handshake");
+error!(ERR_DUPLICATE_UUID, 0x00_003, "uuid already in use");
+// endregion
+
+// region: Subscription Management
+#[rustfmt::skip]
+error!(ERR_WORLD_SUB_GLOBAL_WORLD, 0x01_001, "cannot subscribe to the global world");
+#[rustfmt::skip]
+error!(ERR_WORLD_UNSUB_GLOBAL_WORLD, 0x01_002, "cannot unsubscribe from the global world");
+#[rustfmt::skip]
+error!(ERR_AREA_SUB_GLOBAL_WORLD, 0x01_003, "cannot subscribe to an area inside the global world");
+#[rustfmt::skip]
+error!(ERR_AREA_UNSUB_GLOBAL_WORLD, 0x01_004, "cannot unsubscribe from an area inside the global world");
+
+error_func!(err_invalid_world_name, 0x01_005);
+// endregion
