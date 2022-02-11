279cbd910ecc8907c3ec519c39b5c178300976ee
Jack Baron <jackmbaron@gmail.com>
2022-02-11 14:27:07 +0000

    handle heartbeats

--- worldql_server/src/processing/heartbeat.rs
+++ worldql_server/src/processing/heartbeat.rs
@@ -1,5 +1,6 @@
 use color_eyre::Result;
 use uuid::Uuid;
+use worldql_messages::client_bound::HeartbeatReply;
 use worldql_messages::server_bound::HeartbeatRequest;
 
 use crate::trace_packet;
@@ -7,10 +8,19 @@ use crate::transport::ThreadPeerMap;
 
 pub(super) async fn handle_heartbeat(
     sender: Uuid,
-    request: &HeartbeatRequest,
+    request: HeartbeatRequest,
     peer_map: &ThreadPeerMap,
 ) -> Result<()> {
     trace_packet!("{:?}", &request);
 
-    todo!()
+    let mut map = peer_map.write().await;
+    if let Some(peer) = map.get_mut(&sender) {
+        peer.update_heartbeat();
+
+        let reply: HeartbeatReply = request.into();
+        // TODO: Handle errors
+        let _ = peer.send_message(&reply.into()).await;
+    }
+
+    Ok(())
 }
