2f3369bf63a207d97943a13aac197c27d87064b9
Jack Baron <jackmbaron@gmail.com>
2022-02-11 15:21:45 +0000

    remove display impl for peer

--- worldql_server/src/transport/peer.rs
+++ worldql_server/src/transport/peer.rs
@@ -1,4 +1,3 @@
-use std::fmt::Display;
 use std::net::SocketAddr;
 use std::time::{Duration, Instant};
 
@@ -54,12 +53,6 @@ impl PartialEq for dyn Peer {
     #[inline]
     fn eq(&self, other: &Self) -> bool {
         self.uuid() == other.uuid()
-    }
-}
-
-impl Display for dyn Peer {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
     }
 }
 
