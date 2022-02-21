1cd68928c91c02cf68d2e2fe5a177f4b0372af94
Jack Baron <jackmbaron@gmail.com>
2022-02-21 13:22:06 +0000

    fix error mapping

--- worldql_server/src/database/error.rs
+++ worldql_server/src/database/error.rs
@@ -2,15 +2,16 @@ use worldql_messages::client_bound::Error;
 
 use crate::errors::err_generic_database_error;
 
-pub(super) trait IntoClientError {
-    #[must_use]
-    fn err(self) -> Error;
+pub(super) trait ResultExt<T> {
+    fn or_client_err(self) -> Result<T, Error>;
 }
 
-impl IntoClientError for sqlx::Error {
+impl<T> ResultExt<T> for Result<T, sqlx::Error> {
     #[inline]
-    fn err(self) -> Error {
-        let message = self.to_string();
-        err_generic_database_error(message)
+    fn or_client_err(self) -> Result<T, Error> {
+        self.map_err(|error| {
+            let message = error.to_string();
+            err_generic_database_error(message)
+        })
     }
 }
