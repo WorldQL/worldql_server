2f3ebfe42430915504dc5cc4ad2b6d23ef9f5087
Jack Baron <jackmbaron@gmail.com>
2022-02-22 01:45:28 +0000

    ensure valid world names for RecordClear

--- worldql_server/src/processing/record_clear.rs
+++ worldql_server/src/processing/record_clear.rs
@@ -1,11 +1,14 @@
 use color_eyre::Result;
+use tracing::debug;
 use uuid::Uuid;
 use worldql_messages::client_bound::{ClientMessageReply, RecordClearReply, Status};
 use worldql_messages::server_bound::RecordClearRequest;
 
 use crate::database::DatabaseClient;
+use crate::errors::err_invalid_world_name;
 use crate::trace_packet;
 use crate::transport::ThreadPeerMap;
+use crate::utils::sanitize_world_name;
 
 pub(super) async fn handle_record_clear(
     sender: Uuid,
@@ -34,16 +37,26 @@ async fn process_message(
 ) -> Status<RecordClearReply> {
     match request {
         RecordClearRequest::ByWorld { world_name } => {
-            let status = match db.clear_records_in_world(&world_name).await {
+            if let Some(error) = sanitize_world_name(&world_name) {
+                debug!(
+                    "peer {} sent invalid world name: {} ({})",
+                    &sender, &world_name, error
+                );
+
+                let message = format!("invalid world name: {}", error);
+                let error = err_invalid_world_name(message);
+
+                return error.into();
+            }
+
+            match db.clear_records_in_world(&world_name).await {
                 Err(error) => error.into(),
 
                 Ok(affected) => {
                     let reply = RecordClearReply::new(affected);
                     Status::Ok(reply)
                 }
-            };
-
-            status.into()
+            }
         }
 
         RecordClearRequest::ByArea {
@@ -51,16 +64,26 @@ async fn process_message(
             pos_1,
             pos_2,
         } => {
-            let status = match db.clear_records_in_area(&world_name, pos_1, pos_2).await {
+            if let Some(error) = sanitize_world_name(&world_name) {
+                debug!(
+                    "peer {} sent invalid world name: {} ({})",
+                    &sender, &world_name, error
+                );
+
+                let message = format!("invalid world name: {}", error);
+                let error = err_invalid_world_name(message);
+
+                return error.into();
+            }
+
+            match db.clear_records_in_area(&world_name, pos_1, pos_2).await {
                 Err(error) => error.into(),
 
                 Ok(affected) => {
                     let reply = RecordClearReply::new(affected);
                     Status::Ok(reply)
                 }
-            };
-
-            status.into()
+            }
         }
     }
 }
