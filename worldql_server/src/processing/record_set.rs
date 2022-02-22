1862e653ffef9a2b2dd32098c7d2c11c57ceb8e8
Jack Baron <jackmbaron@gmail.com>
2022-02-22 01:50:05 +0000

    ensure valid world names on Vec<PartialRecord>

--- worldql_server/src/processing/record_set.rs
+++ worldql_server/src/processing/record_set.rs
@@ -1,11 +1,14 @@
 use color_eyre::Result;
+use tracing::debug;
 use uuid::Uuid;
 use worldql_messages::client_bound::{ClientMessageReply, RecordSetReply, Status};
 use worldql_messages::server_bound::RecordSetRequest;
 
 use crate::database::DatabaseClient;
+use crate::errors::err_invalid_world_name;
 use crate::trace_packet;
 use crate::transport::ThreadPeerMap;
+use crate::utils::sanitize_world_name;
 
 pub(super) async fn handle_record_set(
     sender: Uuid,
@@ -32,6 +35,22 @@ async fn process_message(
     request: RecordSetRequest,
     db: &DatabaseClient,
 ) -> Status<RecordSetReply> {
+    let sanitize_error = request.records.iter().find_map(|record| {
+        sanitize_world_name(&record.world_name).map(|error| (error, &record.world_name))
+    });
+
+    if let Some((error, world_name)) = sanitize_error {
+        debug!(
+            "peer {} sent invalid world name: {} ({})",
+            &sender, world_name, error
+        );
+
+        let message = format!("invalid world name: {}", error);
+        let error = err_invalid_world_name(message);
+
+        return error.into();
+    }
+
     match db.set_records(request.records).await {
         Err(error) => error.into(),
 
